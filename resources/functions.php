50a722a2b28798b06f9a409d861db330214759a7
Papp László <papp.lazslo.web@gmail.com>
2020-08-04 21:20:30 +0200

    helper functions

--- resources/functions.php
+++ resources/functions.php
@@ -1,3 +1,35 @@
 <?php
 
+function redirect($location){
+    header("Location: $location");
+
+}
+
+function query($sql) {
+
+    global $connect;
+
+    return mysqli_query($connect, $sql);
+}
+
+function confirm($result){
+    global $connect;
+    if(!$result){
+  
+        die("Adatbázis hiba:" . mysqli_error($connect));
+
+    }
+}
+
+function escape_string($string){
+global $connect;
+
+return mysqli_real_escape_string($connect, $string);
+
+}
+
+function fetch_array($result){
+    return mysqli_fetch_array($result);
+}
+
 ?>
--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -1,8 +1,24 @@
 <div class="col-md-3">
     <p class="lead">Shop Name</p>
     <div class="list-group">
-        <a href="category.html" class="list-group-item">Category 1</a>
-            <a href="#" class="list-group-item">Category 2</a>
-              <a href="#" class="list-group-item">Category 3</a>
+     <?php
+     $query = "SELECT * FROM categories";
+     $send_query = mysqli_query($connect, $query);
+     confirm($send_query);
+     
+     
+     while($row = mysqli_fetch_array($send_query)) {
+        $cat_title = $row['cat_title'];
+        echo "<a href='#' class='list-group-item'>{$cat_title}</a>";
+     }
+     
+     
+     ?>
+
+
+
+
+
+        
                     </div>
             </div>
