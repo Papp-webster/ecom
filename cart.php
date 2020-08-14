5f58c655d4933571bb26735ad834f89e2aa1007a
Papp László <papp.lazslo.web@gmail.com>
2020-08-14 15:59:42 +0200

    paypal

--- cart.php
+++ cart.php
@@ -53,6 +53,11 @@ function cart() {
 
     $total = 0;
     $item_quantity = 0;
+    $item_name = 1;
+    $item_number = 1;
+    $amount = 1;
+    $quantity = 1;
+    
 
     foreach ($_SESSION as $name => $value) {
 
@@ -86,10 +91,18 @@ function cart() {
                 <td><a href="cart.php?delete={$p_id}" class="btn btn-danger"><span class='glyphicon glyphicon-remove'></span></a></td>
               
             </tr>
-
+            <input type="hidden" name="item_name_{$item_name}" value="hat">
+            <input type="hidden" name="item_number_{$item_number}" value="123">
+            <input type="hidden" name="amount_{$amount}" value="15.00">
+            <input type="hidden" name="quantity_{$quantity}" value="15.00">
      DELIMITER;
 
      echo $cart;
+     
+     $item_name++;
+     $item_number++;
+     $amount++;
+     $quantity++;
     
       }
 
