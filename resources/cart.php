f7bcd85fa1bcdd1340b9131629fc9688cf1024a9
Papp László <papp.lazslo.web@gmail.com>
2020-08-26 20:28:47 +0200

    cart fixed

--- resources/cart.php
+++ resources/cart.php
@@ -65,7 +65,7 @@ function cart() {
                
         if(substr($name, 0, 8) == "product_") {
 
-            $length = strlen($name - 8);
+            $length = strlen($name);
 
             $id = substr($name, 8, $length);
 
